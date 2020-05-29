151c1f96d7bbc45c8bd5608dfbc6b7f1955a8970
cfprice3 <cfprice3@gmail.com>
2020-05-29 16:18:35 -0400

    added notes and fixed deprecation warning message.

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+
+// const to require packages
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
+// const to require paths to routes folder
 const apiRoutes = require("./routes/api");
 const viewRoutes = require("./routes/views");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+// connects to the mongo database 'workout'.  'useInifiedTopology: true' fixes the deprecation warning when running the server.
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useUnifiedTopology: true });
+
