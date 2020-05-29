a457ea9f6814052fd6040173ecf787270a19669d
cfprice3 <cfprice3@gmail.com>
2020-05-29 16:49:52 -0400

    created content for exercises in workout schema

--- models/Workout.js
+++ models/Workout.js
@@ -4,7 +4,29 @@ const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema ({
     day: {type: Date, required: true},
-    exercises:[]
+    exercises:
+    [
+        {
+            type: {
+                type: String
+            },
+            name:{
+                type: String
+            },
+            duration: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                tpye: Number
+            }
+        }
+    ]
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
